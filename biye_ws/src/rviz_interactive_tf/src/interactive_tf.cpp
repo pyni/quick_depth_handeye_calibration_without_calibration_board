1e2ed25139e1f755dfe2c6d8e378be9537073076
pyni <1208336359@qq.com>
2021-04-08 16:03:54 +0800

    Add files via upload

---
+++ biye_ws/src/rviz_interactive_tf/src/interactive_tf.cpp
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2016, Lucas Walter
+ *
+ * Create a tf in rviz with a specified parent and name,
+ * drag it around and rotate it with interactive marker controls.
+ */
+
+#include <boost/bind.hpp>
+#include <geometry_msgs/Pose.h>
+#include <interactive_markers/interactive_marker_server.h>
+#include <ros/ros.h>
+#include <string>
+#include <tf/transform_broadcaster.h>
+#include <visualization_msgs/InteractiveMarker.h>
+
+void testFeedback(
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback)
+{}
+class InteractiveTf
+{
+  ros::NodeHandle nh_;
+  boost::shared_ptr<interactive_markers::InteractiveMarkerServer> server_;
+
+  void processFeedback(unsigned ind, const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+  visualization_msgs::InteractiveMarker int_marker_;
+
+  geometry_msgs::Pose pose_;
+  tf::TransformBroadcaster br_;
+  std::string parent_frame_;
+  std::string frame_;
+  void updateTf(int, const ros::TimerEvent& event);
+  ros::Timer tf_timer_;
+
+public:
+  InteractiveTf();
+  ~InteractiveTf();
+};
+
+InteractiveTf::InteractiveTf() :
+  parent_frame_("map"),
+  frame_("interactive_tf")
+{
+  server_.reset(new interactive_markers::InteractiveMarkerServer("interactive_tf"));
+  pose_.orientation.w = 1.0;
+
+  // TODO(lucasw) need way to get parameters out- tf echo would work
+  float scale_ = 1.0;
+  ros::param::get("~scale", scale_);
+  ros::param::get("~parent_frame", parent_frame_);
+  ros::param::get("~frame", frame_);
+
+  int_marker_.header.frame_id = parent_frame_;
+  // http://answers.ros.org/question/262866/interactive-marker-attached-to-a-moving-frame/
+  // putting a timestamp on the marker makes it not appear
+  // int_marker_.header.stamp = ros::Time::now();
+  int_marker_.name = "interactive_tf";
+  int_marker_.description = "control a tf with 6dof";
+  int_marker_.pose = pose_;
+  int_marker_.scale = scale_;
+
+  {
+  visualization_msgs::InteractiveMarkerControl control;
+
+  // TODO(lucasw) get roll pitch yaw and set as defaults
+
+  control.orientation.w = 1;
+  control.orientation.x = 1;
+  control.orientation.y = 0;
+  control.orientation.z = 0;
+  control.name = "rotate_x";
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+  int_marker_.controls.push_back(control);
+  control.name = "move_x";
+  // TODO(lucasw) how to set initial values?
+  // double x = 0.0;
+  // ros::param::get("~x", x);
+  // control.pose.position.x = x;
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+  int_marker_.controls.push_back(control);
+  // control.pose.position.x = 0.0;
+
+  control.orientation.w = 1;
+  control.orientation.x = 0;
+  control.orientation.y = 1;
+  control.orientation.z = 0;
+  control.name = "rotate_z";
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+  int_marker_.controls.push_back(control);
+  control.name = "move_z";
+  // double z = 0.0;
+  // control.pose.position.z = ros::param::get("~z", z);
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+  int_marker_.controls.push_back(control);
+  // control.pose.position.z = 0.0;
+
+  control.orientation.w = 1;
+  control.orientation.x = 0;
+  control.orientation.y = 0;
+  control.orientation.z = 1;
+  control.name = "rotate_y";
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+  int_marker_.controls.push_back(control);
+  control.name = "move_y";
+  // double y = 0.0;
+  // control.pose.position.z = ros::param::get("~y", y);
+  control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+  int_marker_.controls.push_back(control);
+  // control.pose.position.y = 0.0;
+  }
+
+  server_->insert(int_marker_);
+  // Can't seem to get rid of the 0, _1 parameter
+  server_->setCallback(int_marker_.name,
+      boost::bind(&InteractiveTf::processFeedback, this, 0, _1));
+  // server_->setCallback(int_marker_.name, testFeedback);
+
+  server_->applyChanges();
+
+  tf_timer_ = nh_.createTimer(ros::Duration(0.05),
+      boost::bind(&InteractiveTf::updateTf, this, 0, _1));
+}
+
+InteractiveTf::~InteractiveTf()
+{
+  server_.reset();
+}
+
+void InteractiveTf::updateTf(int, const ros::TimerEvent& event)
+{
+  tf::Transform transform;
+  transform.setOrigin(tf::Vector3(pose_.position.x, pose_.position.y, pose_.position.z));
+  transform.setRotation(tf::Quaternion(pose_.orientation.x,
+      pose_.orientation.y,
+      pose_.orientation.z,
+      pose_.orientation.w));
+  br_.sendTransform(tf::StampedTransform(transform, ros::Time::now(),
+      parent_frame_, frame_));
+}
+
+void InteractiveTf::processFeedback(
+    unsigned ind,
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr& feedback)
+{
+  ROS_DEBUG_STREAM(feedback->header.frame_id);
+  pose_ = feedback->pose;
+  ROS_DEBUG_STREAM(feedback->control_name);
+  ROS_DEBUG_STREAM(feedback->event_type);
+  ROS_DEBUG_STREAM(feedback->mouse_point);
+  // TODO(lucasw) all the pose changes get handled by the server elsewhere?
+  server_->applyChanges();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "interactive_tf");
+  InteractiveTf interactive_tf;
+  ros::spin();
+  return 0;
+}
---
+++ biye_ws/src/visualization_tutorials/rviz_python_tutorial/CHANGELOG.rst
@@ -0,0 +1,38 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rviz_python_tutorial
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.11.0 (2020-05-13)
+-------------------
+
+0.10.4 (2020-05-13)
+-------------------
+* Updated RViz import (`#60 <https://github.com/ros-visualization/visualization_tutorials/issues/60>`_)
+* Updated to use ``catkin_install_python()`` (`#59 <https://github.com/ros-visualization/visualization_tutorials/issues/59>`_)
+* Updated required CMake version to avoid CMP0048 warning (`#57 <https://github.com/ros-visualization/visualization_tutorials/issues/57>`_)
+* Contributors: Alejandro Hernández Cordero, Shane Loretz
+
+0.10.3 (2018-05-09)
+-------------------
+* Fixed QWidget not defined bug in rviz_python_tutorial (`#41 <https://github.com/ros-visualization/visualization_tutorials/issues/41>`_)
+* Contributors: Zihan Chen
+
+0.10.2 (2018-01-05)
+-------------------
+
+0.10.1 (2016-04-21)
+-------------------
+
+0.10.0 (2016-04-21)
+-------------------
+
+0.9.2 (2015-09-21)
+------------------
+
+0.9.1 (2015-01-26)
+------------------
+
+0.9.0 (2014-03-24)
+------------------
+* set myself (william) as maintainer
+* Contributors: William Woodall
